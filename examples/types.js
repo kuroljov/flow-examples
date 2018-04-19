1f8312614bb39d6ec8000821479110f903306055
Viktor <viktor.konsta@gmail.com>
2018-04-19 16:21:43 +0300

    Add types

---
+++ examples/types.js
@@ -0,0 +1,23 @@
+
+const id = 1373
+const name = 'Viktor'
+const list = [1, 2, 3]
+const listAny = [1, 'foo', {}, []]
+const me = { id, name }
+const now = new Date()
+const uniq = Symbol('uniq')
+
+function sum (a, b) {
+  return a + b
+}
+
+class User {
+  constructor (firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  get name () {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
